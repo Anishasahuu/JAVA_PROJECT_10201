4c591fe8f0ec959814bee49ff4dcf151cfb97997
Anirudh Kumar <anirudh9123@gmail.com>
2026-06-12 22:04:01 +0530

    User Class

--- ChatRoom.java
+++ ChatRoom.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+class User {
+    private String username;
+    private boolean isOnline;
+
+    User(String username) {
+        this.username = username;
+        this.isOnline = true;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    boolean isOnline() {
+        return isOnline;
+    }
+}
+
 public class ChatRoom {
     public static void main (String [] args){
         
